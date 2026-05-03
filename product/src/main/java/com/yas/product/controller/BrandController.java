0e28c1494fa8a319d7c443cd316bbab1dfaa8813
TuanAnh20-10 <tuananhpq20@gmail.com>
2026-05-03 19:34:00 +0700

    test

--- product/src/main/java/com/yas/product/controller/BrandController.java
+++ product/src/main/java/com/yas/product/controller/BrandController.java
@@ -30,6 +30,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+//hi
 @RestController
 public class BrandController {
     private static final Logger log = LoggerFactory.getLogger(BrandController.class);
